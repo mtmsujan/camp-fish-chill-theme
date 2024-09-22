d690fe1786fb03d956bd9171f583b98c0214d277
mtmsujon <mtmsujon@gmail.com>
2024-09-22 12:21:26 +0600

    initial commit import theme boilertamplate

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Camp Fish Chill Theme Customizer
+ *
+ * @package Camp_Fish_Chill
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function camp_fish_chill_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'camp_fish_chill_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'camp_fish_chill_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'camp_fish_chill_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function camp_fish_chill_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function camp_fish_chill_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function camp_fish_chill_customize_preview_js() {
+	wp_enqueue_script( 'camp-fish-chill-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'camp_fish_chill_customize_preview_js' );
